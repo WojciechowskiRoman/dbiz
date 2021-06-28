e05e2a054558d02e30caeeb34f115efd349b3c04
WojciechowskiRoman <roman.vaitsakhouski@gmail.com>
2021-06-28 23:01:24 +0200

    added part of tabs section

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,13 @@ $(function() {
         dots: true,
         fade: true,
     });
+
+    $('.award__link').on('click', function(e) {
+        e.preventDefault();
+        $('.award__link').removeClass('award__link--active');
+        $(this).addClass('award__link--active');
+
+        $('.award__item').fadeOut().css('display', 'none');
+        $($(this).attr('href')).fadeIn().css('display', 'flex');
+    });
 });
