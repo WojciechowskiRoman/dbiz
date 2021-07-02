d9a1eae71669685439e74b837f5a5fb10353b56c
WojciechowskiRoman <roman.vaitsakhouski@gmail.com>
2021-07-02 14:35:17 +0200

    added portfolio page

--- app/js/main.js
+++ app/js/main.js
@@ -62,4 +62,29 @@ $(function() {
         $('.faq__item').fadeIn().css('display', 'none');
         $($(this).attr('href')).fadeIn().css('display', 'flex');
     });
+
+    let list = document.querySelectorAll('.portfolio__button');
+    let item = document.querySelectorAll('.course');
+
+    for(let i = 0; i < list.length; i++) {
+        list[i].addEventListener('click', function() {
+            for(let j = 0; j < list.length; j++) {
+                list[j].classList.remove('portfolio__button--active')
+            }
+            this.classList.add('portfolio__button--active');
+
+            let dataFilter = this.getAttribute('data-filter');
+
+            for(let k = 0; k < item.length; k++) {
+                item[k].classList.remove('active');
+                item[k].classList.add('hide');
+
+                if(item[k].getAttribute('data-item') == dataFilter ||
+                dataFilter == 'all') {
+                    item[k].classList.remove('hide');
+                    item[k].classList.add('active');
+                }
+            }
+        });
+    }
 });
